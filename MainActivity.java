42bc77c4d9a888ed161c67adc801a3b7a8567b0e
shahistadbest <shahistadbest@gmail.com>
2018-12-26 14:50:38 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,163 @@
+package com.example.hansrajmete.calculator;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+    public static double eval(final String str) {
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
+    }
+
+    public void onClickNum(View v)
+    {
+        Button b = (Button)v;
+        String buttonText = b.getText().toString();
+        String temp=((TextView)findViewById(R.id.textView)).getText().toString();
+        if(temp.length()==1&&temp.charAt(0)=='0')
+        {
+            temp="";
+        }
+        else if(temp.length()!=0)
+        if(temp.charAt(temp.length()-1)=='+'||temp.charAt(temp.length()-1)=='/'||temp.charAt(temp.length()-1)=='*'||temp.charAt(temp.length()-1)=='-')
+        {
+            if(buttonText.charAt(0)=='+'||buttonText.charAt(0)=='/'||buttonText.charAt(0)=='*'||buttonText.charAt(0)=='-')
+                delete(v);
+        }
+        ((TextView)findViewById(R.id.textView)).setText(temp+buttonText);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void equals(View v)
+    {
+        double op=eval(((TextView)findViewById(R.id.textView)).getText().toString());
+        if(op== Math.round(op))
+        ((TextView)findViewById(R.id.textView)).setText(Long.toString(Math.round(op)));
+        else
+            ((TextView)findViewById(R.id.textView)).setText(Double.toString(op));
+    }
+    public void delete(View v)
+    {
+        String s=((TextView)findViewById(R.id.textView)).getText().toString();
+        if(s.length()==1)
+        {
+            ((TextView)findViewById(R.id.textView)).setText("0");
+        }
+        else if(s.length()!=0) {
+            String s1 = s.substring(0, s.length() - 1);
+
+        ((TextView)findViewById(R.id.textView)).setText(s1);}
+    }
+}
